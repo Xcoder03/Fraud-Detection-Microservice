7e8da2a1c3af976884b9ac6919b2d87e920e3740
Ebube Chinda <chindaebube03@gmail.com>
2025-10-20 23:25:12 +0100

    added Transaction model

--- main/java/com/example/Fraud/Detection/Model/Transaction.java
+++ main/java/com/example/Fraud/Detection/Model/Transaction.java
@@ -1,5 +1,28 @@
 package com.example.Fraud.Detection.Model;
 
-public class Transaction {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name= "transaction")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @DecimalMin("0.0")
+    private BigDecimal amount;
+
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+    @ManyToOne(optional = false)
+    private User user;
+
+    private int riskScore;      // computed risk score
+    private boolean highRisk;   // flagged if riskScore exceeds threshold
 }
