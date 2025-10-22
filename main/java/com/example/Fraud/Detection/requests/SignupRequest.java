e9738f4f42f23f5f306dca37e24dc4eb616c3073
Ebube Chinda <chindaebube03@gmail.com>
2025-10-22 18:06:33 +0100

    added signup payload request

---
+++ main/java/com/example/Fraud/Detection/requests/SignupRequest.java
@@ -0,0 +1,22 @@
+package com.example.Fraud.Detection.requests;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SignupRequest {
+
+    @JsonProperty("full_name")
+    private String fullname;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("password")
+    private String password;
+}
